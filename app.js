b9e871e82c0a74179423189f4b70d34c5ceb4240
JHeav49 <159585954+JHeav49@users.noreply.github.com>
2025-09-09 22:57:24 +0000

    First day with js

---
+++ app.js
@@ -0,0 +1,17 @@
+const name = "joshua"
+let age = 16
+const islearningjs = true
+
+console.log(name)
+console.log(age)
+console.log(islearningjs)
+
+age *=2
+console.log(age)
+
+age--
+console.log(age)
+
+let money = 100000
+money++
+money % 5
