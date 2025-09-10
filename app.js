db19d37d7b1897cbc630a441dc5059a04447a3eb
JHeav49 <159585954+JHeav49@users.noreply.github.com>
2025-09-10 23:46:58 +0000

    complete smash charcater assignment

--- app.js
+++ app.js
@@ -15,3 +15,20 @@ console.log(age)
 let money = 100000
 money++
 money % 5
+
+
+let name = "Hatsune Miku"
+let stocks = 3
+let isAlive = true 
+
+console.log("Type of name:", typeof name);
+console.log("Type of stocks:", typeof stocks);
+console.log("Type of isAlive:", typeof isAlive);
+
+stocks = stocks + 1
+stocks = stocks * 5 
+stocks = stocks / 5
+stocks = stocks % 2
+
+isAlive = false
+
